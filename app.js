d22459ce35b8326c9665334bc8286c42fa43e54b
Jonathan Liu <jonathan.liu137@gmail.com>
2017-09-03 01:33:28 -0400

    First commit, start work on html and css

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const question = document.getElementById("question");
+  const enter = document.getElementById("enter");
+  const modal = document.querySelector(".landing-page");
+
+  enter.addEventListener("click", () => {
+    modal.classList.add("hidden");
+  });
+
+  question.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+  });
+
+});
