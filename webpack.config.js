9b5380b3a40dfc7eb2316e6d642b54a1f3798364
Jonathan Liu <jonathan.liu137@gmail.com>
2017-09-07 17:26:49 -0400

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// var path = require("path");
+// var webpack = require("webpack");
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+};
